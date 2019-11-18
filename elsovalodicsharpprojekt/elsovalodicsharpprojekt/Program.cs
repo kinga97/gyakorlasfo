3c64c9b9a06892e10794b751bc15702d89726b64
kinga97 <kinga.kucsera97@gmail.com>
2019-11-18 13:11:14 +0100

    ismét mentés

--- elsovalodicsharpprojekt/elsovalodicsharpprojekt/Program.cs
+++ elsovalodicsharpprojekt/elsovalodicsharpprojekt/Program.cs
@@ -276,7 +276,7 @@ namespace elsovalodicsharpprojekt
                 i++;
             }*/
 
-            /* Plusz feladat: Pozitív szám prím-e? és 18. feladat:
+            /* Plusz feladat: Pozitív szám prím-e? 26. és 18. feladat:
             Console.WriteLine("Kérem a pozitív egész számot: ");
             int bemenoszam = Convert.ToInt32(Console.ReadLine());
 
@@ -380,7 +380,7 @@ namespace elsovalodicsharpprojekt
             }*/
 
             //22. feladat:
-            int szam = 0;
+            /*int szam = 0;
 
             int osszeg = 0;
             while (szam <= 10)
@@ -400,13 +400,171 @@ namespace elsovalodicsharpprojekt
                 }
                 Console.WriteLine($"A beolvasott számok összege: {osszeg}.");
 
+            }*/
+
+            /*Console.WriteLine("2-től 100-ig a prímszámok: ");
+
+            for (int i = 2; i < 101; i++)
+            {
+                if (primszamE(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey(true);
+
+        }*/
+
+
+
+            /* Különálló feladat
+                        //Int32.TryParse használata
+                        int szam = -89;
+                        bool egeszSzam = true;
+
+                        do
+                        {
+                            Console.Write("Kérek egy számot: ");
+                            egeszSzam = Int32.TryParse(Console.ReadLine(), out szam);
+                        } while (!egeszSzam);
+
+
+                        Console.WriteLine($"A bekért szám: {szam}");
+                        */
+
+            /*//23. feladat:
+            string kimenet = "";
+
+            Console.WriteLine("Kérek egy pozitív egész számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            kimenet += szam + " = ";
+
+            while (szam % 2 == 0)
+            {
+                kimenet += 2 + " * ";
+                szam = szam / 2;
+            }
+
+            kimenet += szam;
+
+            Console.WriteLine(kimenet);*/
+
+            /* //24. feladat:
+             string alma = "";
+
+             while (alma != "alma")
+             {
+                 Console.Write("Kérek egy gyümölcsöt: ");
+                 alma = Console.ReadLine();
+             }
+
+             Console.WriteLine("Az alma gyümölcs.");
+             */
+
+
+            //25. feladat:
+            /*string kimenet = "";
+
+            Console.WriteLine("Kérek egy pozitív egész számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            kimenet += szam + " = ";
+            int egesz = 0;
+            int tort = -1;
+
+
+            while ( szam - 3 >= 0)
+            {
+                szam = szam - 3;
+                egesz++;
+                tort = szam;
+            }
+
+            kimenet += egesz + " * 3 +" + tort;
+
+            Console.WriteLine(kimenet);
+            */
+
+            /*
+            //28. és 29. feladat:
+            string kimenet = "";
+
+            Console.WriteLine("Kérek egy pozitív egész számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            kimenet += szam + " = ";
+            int meddig = (int)(Math.Floor(Math.Sqrt(szam))) + 1;
+
+            for (int i = 2; i < meddig; i++)
+            {
+                if (szam % i == 0)
+                {
+                    if (szam == i)
+                    {
+                        kimenet += i;
+                    }
+                    else
+                    {
+                        kimenet += i + " * ";
+                        szam = szam / i;
+                        i--;
+                    }
+                }
             }
 
             
+
+            Console.WriteLine(kimenet);
+            */
+
+            /*
+            //32. feladat:
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{szam} * {i} = {szam * i}");
+            }
+            */
+
+            /*
+            // 33. feladat
+            Console.Write("Kérek egy pozitív egész számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < szam + 1; i++)
+            {
+                for (int j = 1; j < szam + 1; j++)
+                {
+                    Console.SetCursorPosition(3 * (j - 1) +1, i);
+                    Console.Write($"{i * j} ");
+                    
+                }
+                Console.WriteLine();
+            }
+            */
+
+            //34. feladat:
+            for (int i = 10; i < 100; i++)
+            {
+                for (int j = 10; j < 100; j++)
+                {
+
+                }
+                for (int k = 10; k < 100; k++)
+                {
+
+                }
+            }
+
             Console.ReadKey(true);
 
         }
-        static bool primszamE(int szam)
+
+        /*static bool primszamE(int szam)
         {
             bool teljesul = true;
             int osztokSzama = 0;
@@ -424,6 +582,45 @@ namespace elsovalodicsharpprojekt
             {
                 return !teljesul;
             }
+
+     static bool osszegHettel(int szam)
+        {
+            int osszeg = 0;
+            bool hettel = true;
+
+            for (int i = 1; i < szam; i++)
+            {
+                if (szam % i == 0)
+                {
+                    osszeg += i;
+                }
+            }
+
+            if (osszeg % 7 != 0)
+            {
+                hettel = false;
+            }
+
+            return hettel;
         }
+
+        static bool primszamE(int szam)
+        {
+            bool primE = true;
+            int hatar = (int)Math.Floor(Math.Sqrt(szam)) + 1;
+
+            for (int i = 2; i < hatar; i++)
+            {
+                if (szam % i == 0)
+                {
+                    primE = false;
+                    break;
+                }
+            }
+
+            return primE;
+
+
+        }*/
     }
 }
---
+++ refandout/refandout/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace refandout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérem az első számot: ");
+            int elso = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem a második számot: ");
+            int masodik = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérek egy nevet: ");
+            string nev = Console.ReadLine();
+
+            // Console.WriteLine($"A két szám összege: {Osszead(ref elso, ref masodik, ref nev)}");
+            Console.WriteLine($"A két szám összege: {Osszead(out elso, out masodik, out nev)}");
+            Console.WriteLine($"A két szám: {elso}, {masodik}");
+            Console.WriteLine($"A név: {nev}");
+
+
+            Console.ReadKey(true);
+        }
+
+        /*
+         static int Osszead(int a, int b)
+         {
+             //Létrehozom a visszatérő értéket és inicializálom.
+             int osszeg = 0;
+
+             //Kiszámolom a visszatérő értéket.
+             osszeg = a + b;
+
+             //Visszaadom a visszatérő értéket.
+             return osszeg;
+
+             int osszeg = a + b;
+             a = 128;
+             b = -49;
+             return osszeg;
+
+         }
+         */
+
+        //static int Osszead(ref int a, ref int b, ref string nev)
+        static int Osszead(out int a, out int b, out string nev)
+        {
+            a = 0;
+            b = 0;
+            int osszeg = a + b;
+
+            //a = 128;
+            //b = -49;
+            nev = "Gábor";
+            return osszeg;
+
+            /*Console.WriteLine("1-től 100-ig azok a számok, melyek valódi osztóinak összege osztható héttel.");
+            for (int i = 1; i < 101; i++)
+            {
+                if (osszegHettel(i))
+                {
+                    Console.WriteLine($"{i} valódi osztóinak összege osztható 7-tel.");
+                }
+            }*/
+
+            
+        }
+    }
+}
