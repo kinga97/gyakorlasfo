ccdc89d9d9e5f12814351679541b058eb4078fe0
kinga97 <kinga.kucsera97@gmail.com>
2019-11-25 10:07:47 +0100

    megoldás

--- elsovalodicsharpprojekt/elsovalodicsharpprojekt/Program.cs
+++ elsovalodicsharpprojekt/elsovalodicsharpprojekt/Program.cs
@@ -588,9 +588,177 @@ namespace elsovalodicsharpprojekt
             }
             */
 
+            /*
+            //36. feladat:
+            Console.Write("Kérem a sorok számát: ");
+            int sorok = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem az oszlopok számát: ");
+            int oszlopok = Int32.Parse(Console.ReadLine());
 
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O");
+                    }
+                }
+                Console.WriteLine();
+            }
+            */
 
+            /*
+            //37. feladat:
+            Console.Write("Kérek egy természetes számot: ");
+            int a = Int32.Parse(Console.ReadLine());
 
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            */
+
+            /*
+            //38. feladat:
+            1. megoldás:
+            Console.Write("Kérek egy természetes számot: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.SetCursorPosition(a - i, i + 1);
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            2. megoldás:
+            Console.Write("Kérek egy természetes számot: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            { 
+                for (int k = 0; k < a - i; k++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+            */
+
+            /*
+            // 39. feladat:
+            Console.Write("Kérem a sorok számát: ");
+            int sorok = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem az oszlopok számát: ");
+            int oszlopok = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    if ((i == 0) || (i == (sorok - 1)))
+                    {
+                        Console.Write('*');
+                    }
+                    else if ((j == 0) || (j == oszlopok -1)) {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+
+            //Kerettel vízszintesen és függőlegesen is a közepén:
+            Console.Write("Kérem a sorok számát: ");
+            int sorok = Int32.Parse(Console.ReadLine());
+            Console.Write("Kérem az oszlopok számát: ");
+            int oszlopok = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    if ((i == 0) || (i == (sorok - 1)) || (i == (sorok - 1) / 2))
+                    {
+                        Console.Write('*');
+                    }
+                    else if ((j == 0) || (j == oszlopok - 1) || (j == (oszlopok - 1) / 2))
+                    {
+                        Console.Write('*');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+                Console.WriteLine();
+            }
+            */
+
+            /*
+            //41. feladat:
+            for (int i = 65; i < 91; i++)
+            {
+                Console.Write((char)i);
+            }
+            Console.WriteLine();
+            for (int j = 66; j < 91; j++)
+            {
+                Console.Write((char)j);
+            }
+            for (int k = 65; k < 66; k++)
+            {
+                Console.Write((char)k);
+            }
+            for (int n = 66; n < 67; n++)
+            {
+                Console.Write((char)n);
+            }
+            
+        
+            Másik:
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 65 + i; j < 91; j++)
+                {
+                    Console.Write((char)j);
+                }
+                Console.WriteLine();
+            }
+            */
+            for (int i = 0; i < 26; i++)
+            {
+                for (int j = 65 + i; j < 91; j++)
+                {
+                    Console.Write((char)j);
+                }
+                for (int k = 65 + i; k < 65; k++)
+                {
+                    Console.Write((char)k);
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey(true);
 
         }
