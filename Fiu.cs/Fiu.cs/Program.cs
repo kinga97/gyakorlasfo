175c39a88ccec69a191f0b143d88dd93b4d65b59
kucsera.kinga <kucsera.kinga@O16G09>
2020-01-13 08:02:49 +0100

    első mentés

---
+++ Fiu.cs/Fiu.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiu.cs
+{
+    
+            class Fiu : Ember
+        {
+            private bool szemelyiSzamEgyesE;
+
+            public bool SzemelyiSzamEgyesE
+            {
+                get { return szemelyiSzamEgyesE; }
+                set { this.szemelyiSzamEgyesE = value; }
+            }
+
+            public Fiu(int magassag, int testsuly, string szuletesiEv, bool szem) : base(magassag, testsuly, szuletesiEv)
+            {
+                this.szemelyiSzamEgyesE = szem;
+            }
+        }
+    }
+}
