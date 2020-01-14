df24ba96d936d9d43afb5e77d7f963985516bba4
kucsera.kinga <kucsera.kinga@L24G07>
2020-01-14 14:11:08 +0100

    utolsó mentés

---
+++ Sikidomok ugyanaz/Sikidomok ugyanaz/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sikidomok_ugyanaz
+{
+    class Program
+    {
+        static void Kiiras(Negyzet negy)
+        {
+            if (negy is Teglalap)
+            {
+                Console.WriteLine($"A négyzet kerülete: {(negy as Teglalap).KeruletSzamol()}");
+            }
+            else
+            {
+                Console.WriteLine($"A négyzet kerülete: {negy.KeruletSzamol()}");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            /*Negyzet negyzet = new Negyzet(12);
+            Console.WriteLine($"A négyzet kerülete: {negyzet.KeruletSzamol()}");
+            Console.WriteLine($"A négyzet területe: {negyzet.TeruletSzamol()}");
+            Teglalap teglalap = new Teglalap(12, 10);
+            Console.WriteLine($"A téglalap kerülete: {teglalap.KeruletSzamol()}");
+            Console.WriteLine($"A téglalap területe: {teglalap.TeruletSzamol()}");*/
+
+            Negyzet negyzet = new Negyzet(10);
+            Teglalap teglalap = new Teglalap(20, 30);
+
+            Kiiras(negyzet);
+            Kiiras(teglalap);
+
+
+            /*Console.WriteLine($"A négyzet kerülete: {negyzet.KeruletSzamol()}");
+            Console.WriteLine($"A négyzet területe: {negyzet.TeruletSzamol()}");*/
+
+            Console.ReadKey(true);
+        }
+
+        class Negyzet
+        {
+            public int AOldal { get; set; }
+
+            public Negyzet(int aOldal)
+            {
+                this.AOldal = aOldal;
+            }
+
+            public int TeruletSzamol()
+            {
+                return this.AOldal * this.AOldal;
+            }
+
+            public int KeruletSzamol()
+            {
+                return this.AOldal * 4;
+            }
+
+            public void Kiir(Negyzet negy)
+            {
+                Console.WriteLine($"A négyzet kerülete: {negy.KeruletSzamol()}");
+            }
+
+            public Negyzet()
+            {
+
+            }
+        }
+
+        class Teglalap : Negyzet
+        {
+            public int BOldal { get; set; }
+
+            public Teglalap(int aOldal, int bOldal) : base(aOldal)
+            {
+                this.BOldal = bOldal;
+            }
+
+            new public int TeruletSzamol()
+            {
+                return this.AOldal * this.BOldal;
+            }
+
+            new public int KeruletSzamol()
+            {
+                return (this.AOldal + this.BOldal) * 2;
+            }
+
+            new public void Kiir(Negyzet negy)
+            {
+                Console.WriteLine($"A négyzet kerülete:{negy.KeruletSzamol()}");
+            }
+
+            public Teglalap()
+            {
+
+            }
+        }
+
+    }
+}
