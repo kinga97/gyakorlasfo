b0b2a592efad81d539f32bb3652c3a393cc397a4
kucsera.kinga <kucsera.kinga@L24G07>
2020-01-21 15:24:24 +0100

    kész

---
+++ PhoneNumberFormatting/PhoneNumberFormatting/Feladat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumberFormatting
+{
+    class Feladat
+    {
+        public int[] tomb { get; set; }
+
+        public Feladat(int[] a)
+        {
+            this.tomb = a;
+        }
+
+        public string TelefonSzam()
+        {
+            string valt = "";
+           
+
+            valt = $"({this.tomb[0]}{this.tomb[1]}{this.tomb[2]}) {this.tomb[3]}{this.tomb[4]}{this.tomb[5]}-{this.tomb[6]}{this.tomb[7]}{this.tomb[8]}{this.tomb[9]}";
+
+
+            return valt;
+        }
+    }
+}
---
+++ PhoneNumberFormatting/PhoneNumberFormatting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneNumberFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i <= 9; i++)
+            {
+                tomb[i] = rnd.Next(10);
+            }
+
+            Feladat szamok = new Feladat(tomb);
+
+            //Console.WriteLine(Telefonszam(tomb));
+            Console.WriteLine(szamok.TelefonSzam());
+
+            Console.ReadKey(true);
+        }
+
+        static string Telefonszam(int[] tomb)
+        {
+            string valt = "";
+
+            valt = $"({tomb[0]}{tomb[1]}{tomb[2]}) {tomb[3]}{tomb[4]}{tomb[5]}-{tomb[6]}{tomb[7]}{tomb[8]}{tomb[9]}";
+
+
+            return valt;
+        }
+    }
+}
